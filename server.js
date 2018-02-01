427cba6efb36c1bcb6752370a9c6d6af670c2711
Souvik Das <souvik.das@live.in>
2018-02-01 11:43:08 +0530

    New Update

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//Written By Souvik Das 25/01/18
+
 const express = require('express');
 const path = require('path');
 const http = require('http');
@@ -8,15 +10,17 @@ var resultOp;
 var token;
 var token_type;
 
-var set_attributes={};
+var set_attributes = {};
 
-var responseObject={};
+var responseObject = {};
 
 const app = express();
 
 // Parsers for POST data
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -24,246 +28,185 @@ app.use(express.static(path.join(__dirname, 'dist')));
 var fs = require("fs");
 var request = require("request");
 
-var strm="https://img2.carmax.com/stock/mm-honda-accord/500";
+var strm = "https://img2.carmax.com/stock/mm-honda-accord/500";
 
 //Default Api
-app.get ('/', (req, res) => {
-    res.send({"Status":"Welcome.. API up & running"});
+app.get('/', (req, res) => {
+    res.send({
+        "Status": "Welcome.. API up & running"
+    });
 });
 
 
-app.get ('/getPrediction', (req, res) => {
+app.get('/getPrediction', (req, res) => {
 
-  var imgName="/dist/car.jpg";
-  // '/path/to/dest/photo.jpg'
-  const options1 = {
-    //url: 'https://www.cstatic-images.com/stock/900x600/1402518103458.jpg',
-    url:req.query.imgurl,
-    dest:  __dirname + imgName,
-    rejectUnauthorized: false       // Save to /path/to/dest/photo.jpg
-  }
+    var imgName = "/dist/car.jpg";
+    // '/path/to/dest/photo.jpg'
+    const options1 = {
+        //url: 'https://www.cstatic-images.com/stock/900x600/1402518103458.jpg',
+        url: req.query.imgurl,
+        dest: __dirname + imgName,
+        rejectUnauthorized: false // Save to /path/to/dest/photo.jpg
+    }
 
-  download.image(options1)
-    .then(({ filename, image }) => {
-      console.log('File saved to', filename)
-    }).catch((err) => {
-      throw err
-    })
+    download.image(options1)
+        .then(({
+            filename, image
+        }) => {
+            console.log('File saved to', filename)
+        }).catch((err) => {
+            throw err
+        })
 });
-
-
-app.get('/getCarDetails', (req, res) => {
-
-  //console.log("Hello There From Api");
-
-var rpns;
-
-var imgName="/dist/car.jpg";
-// '/path/to/dest/photo.jpg'
-const options1 = {
-  //url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/2014_Honda_Accord_2.4_i-VTEC_sedan_%282016-01-07%29_01.jpg/1200px-2014_Honda_Accord_2.4_i-VTEC_sedan_%282016-01-07%29_01.jpg',
-  url:req.query.imgurl,
-  dest:  __dirname + imgName,
-  rejectUnauthorized: false       // Save to /path/to/dest/photo.jpg
-}
-
-download.image(options1)
-  .then(({ filename, image }) => {
-    console.log('File saved to', filename)
-  }).catch((err) => {
-    throw err
-  })
-
-
-
-var options = { method: 'GET',
-  url: 'https://bauth.blippar.com/token',
-  rejectUnauthorized: false,
-  qs:
-   { grant_type: 'client_credentials',
-     client_id: 'd0e925cea1264c10b6e0cc50107c9d3f',
-     client_secret: '4f34a589116a4d42a1d7df2bc40d053c' },
-  headers:
-   {
-     'cache-control': 'no-cache' } };
-
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
-  var jsonObj = JSON.parse(body);
-  console.log(jsonObj.token_type);
-  token_type=jsonObj.token_type;
-  token=jsonObj.access_token;
-  //console.log(token);
-
-  var options = { method: 'POST',
-    url: 'https://bapi-vs.blippar.com/v1/imageLookup',
-    rejectUnauthorized: false,
-    headers:
-     { 'postman-token': '7a60f864-ce31-09f4-0d80-74e19a9d0892',
-       'cache-control': 'no-cache',
-
-       uniqueid: 'Capgemini999',
-       authorization:token_type+' '+token,
-       //authorization: 'Bearer 9b-NCyWYQ1eL7WWtNiaPtA',
-       'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' },
-    formData:
-     { input_image:
-        { //value: fs.createReadStream(__dirname + imgName),
-          value: fs.createReadStream(__dirname + imgName),
-          options: { filename: 'car.jpg', contentType: null } } } };
-
-  request(options, function (error, response, body) {
-    if (error) throw new Error(error);
-
-    resultOp=JSON.parse(body);
-
-    set_attributes.vehyear=resultOp[0].Note.make;
-    set_attributes.vehmake=resultOp[0].Note.model;
-    set_attributes.vehmodel=resultOp[0].Note.model;
-
-    res.send(set_attributes);
-
-
-  });
-});
-});
-
 
 //Main Api Wrapper
 app.get('/getDetails', (req, res) => {
-var imgName="/dist/car.jpg";
-const options1 = {
-  //url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/2014_Honda_Accord_2.4_i-VTEC_sedan_%282016-01-07%29_01.jpg/1200px-2014_Honda_Accord_2.4_i-VTEC_sedan_%282016-01-07%29_01.jpg',
-  url:req.query.imgurl,
-  dest:  __dirname + imgName,
-  rejectUnauthorized: false
-}
+    var imgName = "/dist/car.jpg";
+    const options1 = {
+        //url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/2014_Honda_Accord_2.4_i-VTEC_sedan_%282016-01-07%29_01.jpg/1200px-2014_Honda_Accord_2.4_i-VTEC_sedan_%282016-01-07%29_01.jpg',
+        url: req.query.imgurl,
+        dest: __dirname + imgName,
+        rejectUnauthorized: false
+    }
 
-download.image(options1)
-  .then(({ filename, image }) => {
-    console.log('File saved to', filename)
-  }).catch((err) => {
-    throw err
-  })
+    download.image(options1)
+        .then(({
+            filename, image
+        }) => {
+            console.log('File saved to', filename)
+        }).catch((err) => {
+            throw err
+        })
 
 
- var options = { method: 'POST',
-   url: 'https://bapi-vs.blippar.com/v1/imageLookup',
-   rejectUnauthorized: false,
-   headers:
-    { 'postman-token': '7a60f864-ce31-09f4-0d80-74e19a9d0892',
-      'cache-control': 'no-cache',
-      uniqueid: 'Capgemini999',
-      //authorization: 'Bearer 9b-NCyWYQ1eL7WWtNiaPtA',
-        authorization:token_type+' '+token,
-      'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' },
-   formData:
-    { input_image:
-       {
-         value: fs.createReadStream(__dirname + imgName),
-         options: { filename: 'car.jpg', contentType: null } } } };
-
- request(options, function (error, response, body) {
-   if (error)
-   console.log(error);
-   resultOp=JSON.parse(body);
-
-   if(resultOp.status!=undefined && resultOp.error.code==605)
-   {
-     var options = { method: 'GET',
-       url: 'https://bauth.blippar.com/token',
-       rejectUnauthorized: false,
-       qs:
-        { grant_type: 'client_credentials',
-          client_id: 'd0e925cea1264c10b6e0cc50107c9d3f',
-          client_secret: '4f34a589116a4d42a1d7df2bc40d053c' },
-       headers:
-        {
-          'cache-control': 'no-cache' } };
-
-     request(options, function (error, response, body) {
-       if (error) throw new Error(error);
-       var jsonObj = JSON.parse(body);
-       console.log(jsonObj.token_type);
-       token_type=jsonObj.token_type;
-       token=jsonObj.access_token;
-       var options = { method: 'POST',
-         url: 'https://bapi-vs.blippar.com/v1/imageLookup',
-         rejectUnauthorized: false,
-         headers:
-          { 'postman-token': '7a60f864-ce31-09f4-0d80-74e19a9d0892',
+    var options = {
+        method: 'POST',
+        url: 'https://bapi-vs.blippar.com/v1/imageLookup',
+        rejectUnauthorized: false,
+        headers: {
+            'postman-token': '7a60f864-ce31-09f4-0d80-74e19a9d0892',
             'cache-control': 'no-cache',
-
             uniqueid: 'Capgemini999',
-            authorization:token_type+' '+token,
             //authorization: 'Bearer 9b-NCyWYQ1eL7WWtNiaPtA',
-            'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' },
-         formData:
-          { input_image:
-             {
-               value: fs.createReadStream(__dirname + imgName),
-               options: { filename: 'car.jpg', contentType: null } } } };
-       request(options, function (error, response, body) {
-           resultOp=JSON.parse(body);
-           console.log("Hiiii");
-           console.log(resultOp);
-         if (error) throw new Error(error);
+            authorization: token_type + ' ' + token,
+            'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
+        },
+        formData: {
+            input_image: {
+                value: fs.createReadStream(__dirname + imgName),
+                options: {
+                    filename: 'car.jpg',
+                    contentType: null
+                }
+            }
+        }
+    };
 
-          if(resultOp.status!=undefined)
-          {
-            res.send({"Status":"Failed"});
-          }
-         else {
+    request(options, function(error, response, body) {
+        if (error)
+            console.log(error);
+        resultOp = JSON.parse(body);
 
+        if (resultOp.status != undefined && resultOp.error.code == 605) {
+            var options = {
+                method: 'GET',
+                url: 'https://bauth.blippar.com/token',
+                rejectUnauthorized: false,
+                qs: {
+                    grant_type: 'client_credentials',
+                    client_id: 'd0e925cea1264c10b6e0cc50107c9d3f',
+                    client_secret: '4f34a589116a4d42a1d7df2bc40d053c'
+                },
+                headers: {
+                    'cache-control': 'no-cache'
+                }
+            };
 
+            request(options, function(error, response, body) {
+                if (error) throw new Error(error);
+                var jsonObj = JSON.parse(body);
+                console.log(jsonObj.token_type);
+                token_type = jsonObj.token_type;
+                token = jsonObj.access_token;
+                var options = {
+                    method: 'POST',
+                    url: 'https://bapi-vs.blippar.com/v1/imageLookup',
+                    rejectUnauthorized: false,
+                    headers: {
+                        'postman-token': '7a60f864-ce31-09f4-0d80-74e19a9d0892',
+                        'cache-control': 'no-cache',
 
-           set_attributes.vehyear=resultOp[0].Note.yeargroup;
-           set_attributes.vehmake=resultOp[0].Note.make;
-           set_attributes.vehmodel=resultOp[0].Note.model;
-           responseObject.set_attributes=set_attributes;
-           res.send(responseObject);
-         }
+                        uniqueid: 'Capgemini999',
+                        authorization: token_type + ' ' + token,
+                        //authorization: 'Bearer 9b-NCyWYQ1eL7WWtNiaPtA',
+                        'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
+                    },
+                    formData: {
+                        input_image: {
+                            value: fs.createReadStream(__dirname + imgName),
+                            options: {
+                                filename: 'car.jpg',
+                                contentType: null
+                            }
+                        }
+                    }
+                };
+                request(options, function(error, response, body) {
+                    resultOp = JSON.parse(body);
+                    console.log(resultOp);
+                    if (error) throw new Error(error);
 
-       });
+                    if (resultOp.status != undefined) {
+                        res.send({
+                            "Status": "Failed"
+                        });
+                    } else {
+                        set_attributes.vehyear = resultOp[0].Note.yeargroup;
+                        set_attributes.vehmake = resultOp[0].Note.make;
+                        set_attributes.vehmodel = resultOp[0].Note.model;
+                        responseObject.set_attributes = set_attributes;
+                        res.send(responseObject);
+                    }
+                });
+            });
+        } else {
+            set_attributes.vehyear = resultOp[0].Note.yeargroup;
+            set_attributes.vehmake = resultOp[0].Note.make;
+            set_attributes.vehmodel = resultOp[0].Note.model;
+            responseObject.set_attributes = set_attributes;
+            res.send(responseObject);
+        }
 
-     });
-   }
-   else {
-     set_attributes.vehyear=resultOp[0].Note.yeargroup;
-     set_attributes.vehmake=resultOp[0].Note.make;
-     set_attributes.vehmodel=resultOp[0].Note.model;
-     responseObject.set_attributes=set_attributes;
-     res.send(responseObject);
-   }
-
- });
+    });
 
 });
 
- function getToken()
- {
-   console.log("Token Initialized");
-   var options = { method: 'GET',
-     url: 'https://bauth.blippar.com/token',
-     rejectUnauthorized: false,
-     qs:
-      { grant_type: 'client_credentials',
-        client_id: 'd0e925cea1264c10b6e0cc50107c9d3f',
-        client_secret: '4f34a589116a4d42a1d7df2bc40d053c' },
-     headers:
-      {
-        'cache-control': 'no-cache' } };
+function getToken() {
+    console.log("Token Initialized");
+    var options = {
+        method: 'GET',
+        url: 'https://bauth.blippar.com/token',
+        rejectUnauthorized: false,
+        qs: {
+            grant_type: 'client_credentials',
+            client_id: 'd0e925cea1264c10b6e0cc50107c9d3f',
+            client_secret: '4f34a589116a4d42a1d7df2bc40d053c'
+        },
+        headers: {
+            'cache-control': 'no-cache'
+        }
+    };
 
-   request(options, function (error, response, body) {
-     if (error) throw new Error(error);
-     var jsonObj = JSON.parse(body);
-     console.log(jsonObj.token_type);
-     token_type=jsonObj.token_type;
-     token=jsonObj.access_token;
-     //console.log(token);
-   });
+    request(options, function(error, response, body) {
+        if (error) throw new Error(error);
+        var jsonObj = JSON.parse(body);
+        console.log(jsonObj.token_type);
+        token_type = jsonObj.token_type;
+        token = jsonObj.access_token;
+        //console.log(token);
+    });
 
- }
+}
 
 //Get port from environment and store in Express.
 const port = process.env.PORT || '3009';
